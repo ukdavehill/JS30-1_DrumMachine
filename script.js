0825e4fc9f5f0ed92d233937dc7a0e6c0e23089f
David Hill <ukdavehill@gmail.com>
2019-01-20 19:06:26 +0000

    added all files required

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);  
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    
+    if(!audio) return; // stop function from running
+    audio.currentTime = 0; // rewind to beginning so can play two keys in quick succession 
+    
+    audio.play();
+    key.classList.add("playing");
+};
+
+function removeTransition(e){
+    if(e.propertyName !== "transform") return; // skip if not transform
+    console.log(this);
+    this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
